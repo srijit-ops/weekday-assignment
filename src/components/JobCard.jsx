b97534b3de27e5fa6a1d849502db18f29924ef34
srijit-ops <srijitasengupta23@gmail.com>
2024-05-06 09:55:06 +0530

    jd expansion added

--- src/components/JobCard.jsx
+++ src/components/JobCard.jsx
@@ -1,7 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Styles from "../styles/JobCard.module.css"
 
 function JobCard({name, jdLink, minExp, about, maxJdSalary, minJdSalary, location, role, logo, jobDetailsFromCompany}) {
+
+  const [expanded, setExpanded] = useState(false)
+
   return (
     <div className={Styles.cardContainer}>
       <p className={Styles.postDate} style={{padding:"0.3rem 0.4rem",borderRadius:"0.6rem", border:"1px solid #e6e6e6", fontSize:"0.7rem", width:"fit-content", marginBottom:"0.9rem"}}>⏳ Posted 10 days ago</p>
@@ -21,9 +24,12 @@ function JobCard({name, jdLink, minExp, about, maxJdSalary, minJdSalary, locatio
           <span> ✅</span>
         </p>
       </div>
+      <div className={expanded===false?Styles.mask:Styles.noMask}>
       <p style={{fontSize:"1rem", fontWeight:500}}>About company:</p>
       <p style={{fontWeight:700, fontSize:"0.9rem", marginBottom:"0.4rem", marginTop:"0.4rem"}}>About us:</p>
       <p style={{fontWeight:400, color:"#000000de", fontSize:"0.8rem"}}>{jobDetailsFromCompany}</p>
+      </div>
+      <p style={{color:"#4943da", width:"100%", textAlign:"center", fontSize:"0.9rem", marginBottom:"0.7rem", marginTop:"0.3rem"}} onClick={()=>setExpanded(!expanded)}>Show {expanded===false?"more":"less"}</p>
       <p style={{color:"#8b8b8b", fontSize:"0.8rem", fontWeight:600, marginTop:"0.4rem", marginBottom:"0.4rem"}}>Minimum experience</p>
       <p style={{fontSize:"0.9rem", color:"000000de"}}>{minExp?minExp:0} years</p>
       <a href={jdLink} style={{borderRadius:"0.5rem", background:"#55efc4", padding:"0.5rem 1rem", color:"black", textDecoration:"none", display:"flex", justifyContent:"center", gap:"0.2rem", fontWeight:600, fontSize:"0.9rem", marginTop:"0.9rem", marginBottom:"0.6rem"}}>
